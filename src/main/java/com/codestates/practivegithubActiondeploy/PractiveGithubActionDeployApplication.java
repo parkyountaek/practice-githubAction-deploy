d76c7188449b8597d0f71c8b3a33e7a72ad38986
Park Yun Taek <yountaek11@gmail.com>
2022-08-05 16:51:10 +0900

    first commit

---
+++ src/main/java/com/codestates/practivegithubActiondeploy/PractiveGithubActionDeployApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practivegithubActiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiveGithubActionDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiveGithubActionDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practivegithubActiondeploy/PractiveGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practivegithubActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiveGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
